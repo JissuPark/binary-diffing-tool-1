8315f885014a3af4f5e6715bf3ad02d8518f4fe3
vhok74 <wogh8732@naver.com>
2019-09-21 22:11:14 +0900

    first commit

---
+++ Export_Pe_Main.py
@@ -0,0 +1,124 @@
+import json
+import filetype
+import numpy as np
+
+import pefile
+import Pe_Rich
+import Pe_Pdb
+import Pe_Rsrc
+
+
+class Pe_Features:
+    def __init__(self, file_name):
+        self.file_name = file_name
+        self.pe=pefile.PE(self.file_name)
+
+    def extract_rich(self):
+        rich = Pe_Rich.ParseRichHeader(self.file_name)
+        xor_key = rich.xorkey
+
+        print(f'XorKey : {xor_key}')
+        print("ProID    name              count")
+        for key in rich.info_list.keys():
+            count = rich.info_list[key]
+            prodid = (key >> 16)
+            prodid_name = Pe_Rich.PRODID_MAP[prodid] if prodid in Pe_Rich.PRODID_MAP else "<unknown>"
+            # print('%6d   %-15s %5d' % (prodid, prodid_name, count))
+
+        return json.dumps(rich.info_list, indent=4)
+
+    def extract_pdb(self):
+        output_data = {}
+        PDB_result = Pe_Pdb.result_all(self.file_name, output_data)
+        return json.dumps(PDB_result, indent=4)
+
+    def extract_rsrc(self):
+        rsrc = Pe_Rsrc.RsrcParser(self.file_name)
+        rsrc.get_resource()
+        return json.dumps(rsrc.resource, indent=4)
+
+    def imphash_data(self):
+        '''
+        imphash : import table의 모든 함수 목록을 하나의 해쉬로
+        get_imphash()함수 : 함수 이름(.dll)을 찾아서 md5해시 후 리턴
+        '''
+
+        if self.pe.get_imphash().upper() == "":
+            return np.nan
+        return self.pe.get_imphash().upper()
+
+    def filetypes(self):
+        '''
+        파일타입 추출
+        '.'을 기준으로 오른 쪽에 있는 것을 파일 타입으로 kind 변수에 저장
+        extension = 파일 타입, mime = 클라이언트에게 전송된 문서의 다양성을 알려주기 위한 메커니즘
+        '''
+        kind = filetype.guess(self.file_name)
+        if kind is None:
+            return np.nan
+        return {kind.extension: kind.mime}
+
+    def Sectioninfo(self):  # section별 데이터의 해시화
+        sections_dict = {}
+        for sections in self.pe.sections:
+            try:
+                # change meaningless parts like '\x00' into empty char
+                sections_dict[sections.Name.decode().replace("\x00", "")] = [sections.get_hash_sha256(),
+                                                                             hex(sections.PointerToRawData)]
+            except:
+                sname = sections.Name.decode('latin-1').encode('utf-8').decode('utf-8').replace('\x00', '')
+                sname = list(sname)
+                # if name doesn't start with '.' then delete that part
+                if sname[0] != '.':
+                    del (sname[0])
+                # if name start with '.' then return sah256 hash value and section's start address
+                sections_dict[''.join(sname)] = [sections.get_hash_sha256(), hex(sections.PointerToRawData)]
+
+        return sections_dict
+
+    def Autoninfo(self):
+        rsrc = Pe_Rsrc.RsrcParser(self.file_name)
+        authentication = rsrc.extractPKCS7()
+        return json.dumps(authentication, indent=4)
+
+    def ImportDll(self):
+        '''
+        함수 목록에 대해서 모두 리스트로 추출
+        ngram으로 유사도 비교 가능
+        '''
+        ImportDlldict = {}
+        try:
+            for entry in self.pe.DIRECTORY_ENTRY_IMPORT:
+                importlists = []
+                for imp in entry.imports:
+                    try:
+                        importlists.append(imp.name.decode())                           # convert function name into string type and append into importlists
+                    except:
+                        continue
+                # importlists에 있는 값들을 ImportDlldict에 각각의 .dll(함수)에 추가
+                ImportDlldict[entry.dll.decode()] = importlists                         #append values of importlists to ImportDlldict
+        except:
+            return np.nan
+        return ImportDlldict
+
+
+if __name__ == "__main__":
+    PATH = r"C:\Users\qkrwl\PycharmProjects\study\Fileis_setup.exe"
+
+    ppe = Pe_Features(PATH)
+    func_list = ppe.ImportDll()
+    imphash = ppe.imphash_data()
+    section = ppe.Sectioninfo()
+    auto = ppe.Autoninfo()
+
+    print("filetype  :: ", ppe.filetypes())
+    print("function list :: ", json.dumps(func_list, indent=4))
+    print("imphash_data :: ", imphash)
+    print("section hash :: ", json.dumps(section, indent=4))
+    print("authentication data :: ", auto)
+
+    print(f'pe rich header info : {ppe.extract_rich()}')
+    print(f'pe pdb  header info : {ppe.extract_pdb()}')
+    print(f'pe resource info : {ppe.extract_rsrc()}')
+
+
---
+++ Pe_Pdb.py
@@ -0,0 +1,58 @@
+import pefile
+import numpy as np
+import struct
+
+class PDB_analyze():
+    def __init__(self, filename):
+        self.filename = filename
+        self.pe = pefile.PE(self.filename)
+
+    def run(self, pe_info_json):
+
+        pdb_data = self.pdb_data()  #
+        pe_info_json['pe_pdb_Name'] = pdb_data['Name']
+        pe_info_json['pe_pdb_GUID'] = pdb_data['GUID']  # pdb(컴파일된 정보 중 하나)컴파일된 환경(프로젝트)의 아이디
+        pe_info_json['pe_pdb_Age'] = pdb_data['Age']  # 코드의 빌드 횟수
+        pe_info_json['pe_pdb_pdbPath'] = pdb_data['Pdbpath']  # 중요***      pdb파일이 있는 경로
+
+        self.pe.close()
+        return pe_info_json
+
+    def pdb_data(self):  # pdb 추출하는 함수
+        if hasattr(self.pe, u"DIRECTORY_ENTRY_DEBUG"):
+            for i in self.pe.DIRECTORY_ENTRY_DEBUG:
+                entry = i.entry
+                #PDB 필드의 signature 값이 RSDS인 것들만
+                if hasattr(entry, "CvSignature"):  # RSDS
+                    name = entry.name
+                    ###
+                    #GUID(컴파일된 환경의 아이디) 정보를 바이트형으로 패킹한 후
+                    guid_data1 = struct.pack('<I', entry.Signature_Data1)
+                    guid_data2 = struct.pack('<H', entry.Signature_Data2)
+                    guid_data3 = struct.pack('<H', entry.Signature_Data3)
+                    #little엔디안으로 int형으로 바꿈
+                    GUID = "%x-%x-%x-%x".upper() % (int.from_bytes(guid_data1, "little"),
+                                                    int.from_bytes(guid_data2, "little"),
+                                                    int.from_bytes(guid_data3, "little"),
+                                                    int.from_bytes(entry.Signature_Data4, "little")
+                                                    )
+                    Age = entry.Age
+
+                    return {"Name": name, "GUID": GUID, "Age": Age, "Pdbpath": entry.PdbFileName.decode()}
+
+                elif hasattr(entry, "CvHeaderSignature"):  # NB10
+                    name = entry.name
+                    GUID = hex(entry.Signature)
+                    Age = entry.Age
+                    Pdbpath = entry.PdbFileName.decode()
+                    return {"Name": name, "GUID": GUID, "Age": Age, "Pdbpath": Pdbpath}
+
+                else:
+                    return {"Name": np.nan, "GUID": np.nan, "Age": np.nan, "Pdbpath": np.nan}
+        else:
+            return {"Name": np.nan, "GUID": np.nan, "Age": np.nan, "Pdbpath": np.nan}
+
+def result_all(filename, output_data):
+    pe_anal = PDB_analyze(filename)
+    output_data = pe_anal.run(output_data)
+    return output_data
