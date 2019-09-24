1eb66c71bc232d1e3b9f433a92d55524196dfda9
Jisu Park <qkrwltn9412@gmail.com>
2019-09-24 23:36:04 +0900

    Complete flowchart analyze

--- Analzer_Engine/Analyzer_main.py
+++ Analzer_Engine/Analyzer_main.py
@@ -64,39 +64,6 @@ class AnalyzeSimilarity:
 #############################################################
 
 
-def cmp_hash(self, stand_hash, target_hash):
-    '''
-    해쉬값을 넣어주면 비교해서 얼마나 같은지 점수로 반환해주는 함수 - 현목이가 짰음 (추가요망)
-    :param stand_hash: stand value for compare
-    :param target_hash: target value for compare
-    :return: True or False decided by comparing between standard and target hashes
-    '''
-    cmp_hash_score = 0
-    return cmp_hash_score
-
-
-def cmp_ngarm(self, stand_str, target_str):
-    '''
-    문자열을 넣어주면 비교해서 얼마나 같은지 점수로 반환해주는 함수 - 현목이가 짰음 (추가요망)
-    :param stand_str: stand string value for compare
-    :param targe_str: target string value for compare
-    :return: score calculated by comparing between standard and target strings
-    '''
-    cmp_str_score = 0
-    return cmp_str_score
-
-
-def cmp_data(self, stand_data, target_data):
-    '''
-    데이터를 넣어주면 비교해서 얼마나 같은지 점수로 반환해주는 함수 - ssdeep/sdhash/tlsh 사용 예정
-    :param stand_data: standard data value for compare
-    :param target_data: target data value for compare
-    :return: score calculated by comparing between standard and target data
-    '''
-    cmp_data_score = 0
-    return cmp_data_score
-
-
 # 이대로 완성된다면 main 코드는 이게 전부임
 if __name__ == "__main__":
     print('==========================START MAIN==========================')
