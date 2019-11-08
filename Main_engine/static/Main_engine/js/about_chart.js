5c3ee72cf8dee7f990119fbcc873b4e8abeb5b32
vhok74 <wogh8732@naver.com>
2019-11-08 19:00:12 +0900

    layout.html 수정

---
+++ Main_engine/static/Main_engine/js/about_chart.js
@@ -0,0 +1,267 @@
+
+var ctx = document.getElementById('myChart').getContext('2d');
+var ctx1 = document.getElementById('myPieChart1')
+var ctx2 = document.getElementById('myPieChart2')
+var ctx3 = document.getElementById('myPieChart3')
+var ctx4 = document.getElementById('myPieChart4')
+var ctx5 = document.getElementById('myPieChart5')
+var ctx6 = document.getElementById('myPieChart6')
+var ctx7 = document.getElementById('myPieChart7')
+var ctx8 = document.getElementById('myPieChart8')
+
+var myChart = new Chart(ctx, {
+  type: 'bar',
+  data: {
+      labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange', 'Red', 'Blue', 'Yellow', 'Green', 'Purple'],
+      datasets: [{
+          label: '#Votes',
+          data: [12, 19, 3, 5, 2, 3, 12, 19, 3, 5, 96], /*실제 값-> y축 설정을 따로 해주지 않으면 해당 데이터 셋에서 가장 높은 값을 기준으로 y축을 잡음 */
+          backgroundColor:  'rgba(255, 206, 86)' ,
+          borderColor: 'rgba(255, 206, 86)' ,
+          borderWidth: 1 /*바 표시선 굵기*/
+      }]
+  },
+  /*option 부분 */
+  options: {
+        responsive: false, /*기본 +5 : 이거 false로 설정 안하면 cavanas에 설정한 값과 달리 화면에 꽉차는 반응형으로 만들어 짐*/
+        scales: {
+            yAxes: [{ /*yAxs : y축에 관련된 옵션*/
+                ticks: {
+                    beginAtZero: true /*0부터 표기할지 정하는 거*/
+                }
+            }]
+        },
+    },
+});
+
+var myPieChart1 = new Chart(ctx1, {
+    type: 'pie',
+  data: {
+    labels: ['OK', 'WARNING', 'CRITICAL', 'UNKNOWN'],
+    datasets: [{
+      label: '# of Tomatoes',
+      data: [12, 19, 3, 5],
+      backgroundColor: [
+        'rgba(255, 99, 132, 0.5)',
+        'rgba(54, 162, 235, 0.2)',
+        'rgba(255, 206, 86, 0.2)',
+        'rgba(75, 192, 192, 0.2)'
+      ],
+      borderColor: [
+        'rgba(255,99,132,1)',
+        'rgba(54, 162, 235, 1)',
+        'rgba(255, 206, 86, 1)',
+        'rgba(75, 192, 192, 1)'
+      ],
+      borderWidth: 1
+    }]
+  },
+  options: {
+   	cutoutPercentage: 40,
+    responsive: false,
+
+  }
+ });
+
+var myPieChart2 = new Chart(ctx2, {
+    type: 'pie',
+  data: {
+    labels: ['OK', 'WARNING', 'CRITICAL', 'UNKNOWN'],
+    datasets: [{
+      label: '# of Tomatoes',
+      data: [12, 19, 3, 5],
+      backgroundColor: [
+        'rgba(255, 99, 132, 0.5)',
+        'rgba(54, 162, 235, 0.2)',
+        'rgba(255, 206, 86, 0.2)',
+        'rgba(75, 192, 192, 0.2)'
+      ],
+      borderColor: [
+        'rgba(255,99,132,1)',
+        'rgba(54, 162, 235, 1)',
+        'rgba(255, 206, 86, 1)',
+        'rgba(75, 192, 192, 1)'
+      ],
+      borderWidth: 1
+    }]
+  },
+  options: {
+   	cutoutPercentage: 40,
+    responsive: false,
+
+  }
+  });
+
+var myPieChart3 = new Chart(ctx3, {
+  type: 'pie',
+  data: {
+    labels: ['OK', 'WARNING', 'CRITICAL', 'UNKNOWN'],
+    datasets: [{
+      label: '# of Tomatoes',
+      data: [12, 19, 3, 5],
+      backgroundColor: [
+        'rgba(255, 99, 132, 0.5)',
+        'rgba(54, 162, 235, 0.2)',
+        'rgba(255, 206, 86, 0.2)',
+        'rgba(75, 192, 192, 0.2)'
+      ],
+      borderColor: [
+        'rgba(255,99,132,1)',
+        'rgba(54, 162, 235, 1)',
+        'rgba(255, 206, 86, 1)',
+        'rgba(75, 192, 192, 1)'
+      ],
+      borderWidth: 1
+    }]
+  },
+  options: {
+   	cutoutPercentage: 40,
+    responsive: false,
+
+  }
+  });
+
+var myPieChart4 = new Chart(ctx4, {
+  type: 'pie',
+  data: {
+    labels: ['OK', 'WARNING', 'CRITICAL', 'UNKNOWN'],
+    datasets: [{
+      label: '# of Tomatoes',
+      data: [12, 19, 3, 5],
+      backgroundColor: [
+        'rgba(255, 99, 132, 0.5)',
+        'rgba(54, 162, 235, 0.2)',
+        'rgba(255, 206, 86, 0.2)',
+        'rgba(75, 192, 192, 0.2)'
+      ],
+      borderColor: [
+        'rgba(255,99,132,1)',
+        'rgba(54, 162, 235, 1)',
+        'rgba(255, 206, 86, 1)',
+        'rgba(75, 192, 192, 1)'
+      ],
+      borderWidth: 1
+    }]
+  },
+  options: {
+   	cutoutPercentage: 40,
+    responsive: false,
+
+  }
+});
+
+var myPieChart5 = new Chart(ctx5, {
+  type: 'pie',
+  data: {
+    labels: ['OK', 'WARNING', 'CRITICAL', 'UNKNOWN'],
+    datasets: [{
+      label: '# of Tomatoes',
+      data: [12, 19, 3, 5],
+      backgroundColor: [
+        'rgba(255, 99, 132, 0.5)',
+        'rgba(54, 162, 235, 0.2)',
+        'rgba(255, 206, 86, 0.2)',
+        'rgba(75, 192, 192, 0.2)'
+      ],
+      borderColor: [
+        'rgba(255,99,132,1)',
+        'rgba(54, 162, 235, 1)',
+        'rgba(255, 206, 86, 1)',
+        'rgba(75, 192, 192, 1)'
+      ],
+      borderWidth: 1
+    }]
+  },
+  options: {
+   	cutoutPercentage: 40,
+    responsive: false,
+
+  }
+});
+
+var myPieChart6 = new Chart(ctx6, {
+  type: 'pie',
+  data: {
+    labels: ['OK', 'WARNING', 'CRITICAL', 'UNKNOWN'],
+    datasets: [{
+      label: '# of Tomatoes',
+      data: [12, 19, 3, 5],
+      backgroundColor: [
+        'rgba(255, 99, 132, 0.5)',
+        'rgba(54, 162, 235, 0.2)',
+        'rgba(255, 206, 86, 0.2)',
+        'rgba(75, 192, 192, 0.2)'
+      ],
+      borderColor: [
+        'rgba(255,99,132,1)',
+        'rgba(54, 162, 235, 1)',
+        'rgba(255, 206, 86, 1)',
+        'rgba(75, 192, 192, 1)'
+      ],
+      borderWidth: 1
+    }]
+  },
+  options: {
+   	cutoutPercentage: 40,
+    responsive: false,
+
+  }
+});
+
+var myPieChart7 = new Chart(ctx7, {
+  type: 'pie',
+  data: {
+    labels: ['OK', 'WARNING', 'CRITICAL', 'UNKNOWN'],
+    datasets: [{
+      label: '# of Tomatoes',
+      data: [12, 19, 3, 5],
+      backgroundColor: [
+        'rgba(255, 99, 132, 0.5)',
+        'rgba(54, 162, 235, 0.2)',
+        'rgba(255, 206, 86, 0.2)',
+        'rgba(75, 192, 192, 0.2)'
+      ],
+      borderColor: [
+        'rgba(255,99,132,1)',
+        'rgba(54, 162, 235, 1)',
+        'rgba(255, 206, 86, 1)',
+        'rgba(75, 192, 192, 1)'
+      ],
+      borderWidth: 1
+    }]
+  },
+  options: {
+   	cutoutPercentage: 40,
+    responsive: false,
+
+  }
+});
+
+var myPieChart8 = new Chart(ctx8, {
+  type: 'pie',
+  data: {
+    labels: ['OK', 'WARNING', 'CRITICAL', 'UNKNOWN'],
+    datasets: [{
+      label: '# of Tomatoes',
+      data: [12, 19, 3, 5],
+      backgroundColor: [
+        'rgba(255, 99, 132, 0.5)',
+        'rgba(54, 162, 235, 0.2)',
+        'rgba(255, 206, 86, 0.2)',
+        'rgba(75, 192, 192, 0.2)'
+      ],
+      borderColor: [
+        'rgba(255,99,132,1)',
+        'rgba(54, 162, 235, 1)',
+        'rgba(255, 206, 86, 1)',
+        'rgba(75, 192, 192, 1)'
+      ],
+      borderWidth: 1
+    }]
+  },
+  options: {
+   	cutoutPercentage: 40,
+    responsive: false,
+
+  }
+});
