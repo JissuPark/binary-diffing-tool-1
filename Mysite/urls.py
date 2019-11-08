feffab1583f6c4e45fbad401b12e993443c13bf2
chaeyoungi <icodud0923@naver.com>
2019-11-08 23:12:21 +0900

    sync

--- Mysite/urls.py
+++ Mysite/urls.py
@@ -19,6 +19,6 @@ from django.conf.urls.static import static
 from django.urls import path, include
 
 urlpatterns = [
+    path('', include('Main_engine.urls')),
     path('admin/', admin.site.urls),
-    path('Main_engine/', include('Main_engine.urls'))
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
