c4f8623fe6cbab31fb77f03e500aa31971889a62
skagns9625 <namh5176@naver.com>
2019-12-18 02:28:30 +0900

    p.close()

--- Main_engine/Extract_Engine/PE_feature/extract_pe.py
+++ Main_engine/Extract_Engine/PE_feature/extract_pe.py
@@ -136,9 +136,10 @@ class Pe_Feature:
 
         file_size = os.path.getsize(self.file_name)
         file_size = self.convert_size(file_size)
-        MD5 = hashlib.md5(open(self.file_name, 'rb').read()).hexdigest().upper()
-        sha1 = hashlib.sha1(open(self.file_name, 'rb').read()).hexdigest()
-        sha256 = hashlib.sha256(open(self.file_name, 'rb').read()).hexdigest()
+        with open(self.file_name, 'rb') as p:
+            MD5 = hashlib.md5(p.read()).hexdigest().upper()
+            sha1 = hashlib.sha1(p.read()).hexdigest()
+            sha256 = hashlib.sha256(p.read()).hexdigest()
         ImpHash = imphash.upper()
         ssdeep_hash = ssdeep.hash_from_file(self.file_name)
         TimeStamp = time_info
@@ -178,7 +179,7 @@ class Pe_Feature:
         pe_features = {
             #'file_path': self.file_name,
             'file_name': f_name,
-            'file_hash': hashlib.sha256(open(self.file_name, 'rb').read()).hexdigest(),
+            'file_hash': sha256,
             'imp_hash': imphash,
             'Imports': implist,
             'cmp_section': cmp_section_data,
@@ -222,7 +223,7 @@ class Pe_Feature:
                                timestamp=TimeStamp, year=Year, timenum=TimeInNum,
                                ssdeep=ssdeep_hash, sha_1=sha1, md5=MD5, Targetmachine=mac, EntryPoint=Ent_point,
                                ContainedSections=Section_num, pdbname=pdb_name, pdbguid=pdb_guid, pdbage=pdb_age, pdbpath=pdb_path)
-
+        p.close()
         f_name_hash.close()
 
         return pe_features, pe_features_for_DB
